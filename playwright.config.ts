f1b31f9867fb7413300fe204085d4be44966ee7c
m.r <mr@example.com>
2026-06-08 09:35:36 -0800

    feat: add health check

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -20,10 +20,10 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  timeout: 10*60*1_000, // test timeout
+  timeout: process.env.CI ? 20 * 60 * 1_000 : 10 * 60 * 1_000, // test timeout
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,7 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    actionTimeout: 10*60*1_000,
+    actionTimeout: process.env.CI ? 20 * 60 * 1_000 : 10 * 60 * 1_000,
   },
 
   /* Configure projects for major browsers */
